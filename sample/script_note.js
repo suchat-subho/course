53f2b27ae6c861aedc5742ba78a091ad812e8fed
subhrendu@iitg.ernet.in <subhrendu@idrbt.ac.in>
2025-01-09 12:18:18 +0530

    modified:   microp/README.md 	new file:   sample/GoogleSheetFormat.png 	new file:   sample/GoogleSheetsToJson.gs 	new file:   sample/README.md 	new file:   sample/index.html 	new file:   sample/script_note.js 	new file:   sample/styles_note.css

---
+++ sample/script_note.js
@@ -0,0 +1,39 @@
+//const webAppURL = 'https://script.googleusercontent.com/macros/echo?user_content_key=-nf7HtnAq_0htG1VeTn-eIqjG7DG2zmJVAB9hWC3ghXihLe0aPd4Z5uHfso8dyf3px8_s0aqadWfwTLmhpF3F8T4JLVykrupm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnBgmdpr5BW5TgBjlEPPDofIM2_kNrBC71ajuEfM6JLC4DUaHCjcXJ17FzCh84gbx3gVxNwEBAPUaEcM9aEljYTZS6Ic6skGQNdz9Jw9Md8uu&lib=MiS8nA0IP1S5-p0rxnHc8P-ZNomVPhu3r';
+const webAppURL = 'https://script.google.com/macros/s/AKfycbzM7By2hLDmiGTMKZ6_3ssMo9th6q5gzKMcQN6APpzUgTbJuBjYEp1oy0PUYPOjsh4Iow/exec';
+document.addEventListener('DOMContentLoaded', function() {
+    const notesContainer = document.getElementById('notes-container');
+    const sideNav = document.getElementById('sidenav');
+
+    fetch(webAppURL)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Unable to access JSON');
+            }
+            return response.json();
+        })
+        .then(data => {
+            URI = window.location.href;
+            for (const key in data) {
+                if (data.hasOwnProperty(key)) {
+                    const noteData = data[key];
+                    const note = document.createElement('div');
+                    note.className = 'note';
+                    //note.id="${noteData.ShortURL}"
+                    shortURI = URI + "?url=" + noteData.ShortURL;
+                    note.innerHTML = `
+                        <h3>${noteData.Description}</h3>
+                        <p><strong>Actual URL:</strong> <em>${noteData.URL}</em></p>
+                        <p><strong>Short URL:</strong> <em>${shortURI}</em></p>
+                        <p><a href="${shortURI}" target="_blank" class="button">Go to Short URL</a></p>
+                    `;
+                    notesContainer.appendChild(note);
+                    //const nav = document.createElement('li');
+                    //nav.innerHTML=`<li><a href="#${noteData.ShortURL}">${noteData.Description}</a></li>`;
+                    //sidenav.appendChild(nav);
+                }
+            }
+        })
+        .catch(error => {
+            console.log(error.message);
+        });
+});
