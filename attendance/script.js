3fa6b00d8299027b088ff69bbd763ee6925495f0
subhrendu1987 <subhrendu.chattopadhyay@thapar.edu>
2026-08-13 23:28:07 +0530

    modified:   script.js

--- attendance/script.js
+++ attendance/script.js
@@ -305,18 +305,12 @@ function checkStatusHelp() {
     statusHelp.style.display = "block";
   } else {
     statusHelp.style.display = "none";
-    if (statusMessage.textContent.includes("Attendance marked successfully")) {
-        statusOk.style.display = "block";
-    } else {
-        statusOk.style.display = "none";
-    }
-  }
-}
 
-  if (statusMessage.textContent.includes("has already been claimed")) {
-    statusHelp.style.display = "block";
-  } else {
-    statusHelp.style.display = "none";
+    if (statusMessage.textContent.includes("Attendance marked successfully")) {
+      statusOk.style.display = "block";
+    } else {
+      statusOk.style.display = "none";
+    }
   }
 }
 
