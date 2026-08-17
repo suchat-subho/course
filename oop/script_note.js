de4b729470655e91b2c571680a23dae3bd917ab9
subhrendu1987 <subhrendu.chattopadhyay@thapar.edu>
2026-08-17 10:59:19 +0530

    new file:   icon/imp.gif 	new file:   icon/new.gif 	modified:   oop/script_note.js

--- oop/script_note.js
+++ oop/script_note.js
@@ -18,11 +18,20 @@ document.addEventListener('DOMContentLoaded', function() {
                     const note = document.createElement('div');
                     note.className = 'note';
                     //note.id="${noteData.ShortURL}"
+                    //console.log("noteData:" + Object.keys(noteData)); // View all properties of the object
                     shortURI = URI + "?url=" + noteData.ShortURL;
                     let comment = !noteData.Comment || noteData.Comment.length === 0
                                  ? "" : `<div class="green-box"><p><strong>Note:</strong> ${noteData.Comment}</p></div>`;
+                    
+                    let ticker_new = !noteData.tag || noteData.tag.length === 0 ? "" : noteData.tag.includes("new") ? `<img src="../icon/new.gif" alt="New" style="width: 20px; height: auto;">`: ``;
+                    console.log("new-important:"+noteData.tag);
+                    let ticker_imp = !noteData.tag || noteData.tag.length === 0 ? "" : noteData.tag.includes("imp") ? `<img src="../icon/imp.gif" alt="Important " style="width: 20px; height: auto;">`: ``;
                     note.innerHTML = `
-                        <h3>${noteData.Description}</h3>
+                        <div style="display: flex; align-items: center; gap: 10px; margin-bottom: 10px;">
+                            <h3 style="margin: 0;">${noteData.Description}</h3>
+                            <span>${ticker_new}</span>
+                            <span>${ticker_imp}</span>
+                        </div> 
                         <p><strong>Actual URL:</strong> <em>${noteData.URL}</em></p>
                         <p><strong>Short URL:</strong> <em>${shortURI}</em></p>
                         <p><a href="${shortURI}" target="_blank" class="button">Go to Short URL</a></p>
